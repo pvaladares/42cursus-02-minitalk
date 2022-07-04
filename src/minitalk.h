40b3b3c84483342f79c5f99a0216fe2ac4becf2d
PV <2190926+pvaladares@users.noreply.github.com>
2022-07-04 19:22:22 +0100

    Fixed header and overall check

--- src/minitalk.h
+++ src/minitalk.h
@@ -6,26 +6,33 @@
 /*   By: pvaladar <pvaladar@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 13:57:25 by pvaladar          #+#    #+#             */
-/*   Updated: 2022/07/04 15:12:51 by pvaladar         ###   ########.fr       */
+/*   Updated: 2022/07/04 19:11:32 by pvaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// ======================== DEFINITIONS
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
+# define WAIT_US  100 // usleep()
+
+// ======================== INCLUDES
+
 # include "libft/libft.h" // ft_atoi(), ft_strlen(), ft_putstr(), ft_putnbr()
-# include <unistd.h> // getpid()
-# include <signal.h> // signal()
+# include <unistd.h> // getpid(), write()
+# include <signal.h> // sigemptyset(), sigaction()
 # include <stdlib.h> // exit()
 
-# define WAIT_US				100 //usleep(WAIT_US)
+// ======================== PROTOTYPES
 
+// minitalk.c
 void	configure_sigaction_signals(struct sigaction *sa);
+void	send_int(pid_t pid, int num);
 void	send_char(pid_t pid, char c);
 void	send_bit(pid_t pid, char bit, char flag_to_pause);
-void	send_int(pid_t pid, int num);
-void	ft_putstr(char *s);
-void	ft_putnbr(int n);
+
+// ======================== TYPEDEF and STRUCTURES
 
 /*
   Structure to handle the protocol between server-client
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: pvaladar <pvaladar@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 13:56:24 by pvaladar          #+#    #+#             */
-/*   Updated: 2022/07/04 16:14:56 by pvaladar         ###   ########.fr       */
+/*   Updated: 2022/07/04 19:09:15 by pvaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@
   received (client will then exit), with send_bit() without signal expecting 
   to be received back from client
 */
-static void	is_message_finished(t_protocol *t_server, int *i, pid_t client_pid)
+static void	server_is_message_finished(
+	t_protocol *t_server, int *i, pid_t client_pid)
 {
 	if (t_server->bits == 8 && t_server->flag == 1)
 	{
@@ -45,7 +46,7 @@ static void	is_message_finished(t_protocol *t_server, int *i, pid_t client_pid)
   If yes, the length is printed to stdout and used to allocated in the 
   heap memory a string with the exact size received (plus the null terminator)
 */
-static void	is_str_length_finished(t_protocol *t_server)
+static void	server_is_str_length_finished(t_protocol *t_server)
 {
 	if (t_server->bits == sizeof(int) * 8 && t_server->flag == 0)
 	{
@@ -87,8 +88,8 @@ static void	server_handler(int num, siginfo_t *info, void *context)
 	else if (num == SIGUSR2 && t_server.flag == 1)
 		t_server.data |= 1 << (((sizeof(char) * 8) - 1) - t_server.bits);
 	t_server.bits++;
-	is_message_finished(&t_server, &i, info->si_pid);
-	is_str_length_finished(&t_server);
+	server_is_message_finished(&t_server, &i, info->si_pid);
+	server_is_str_length_finished(&t_server);
 	send_bit(info->si_pid, 0, 0);
 }
 
